6c5c8fade4aa7d82c4ce422d96de5336c0de6277
RashadB88 <Rashad.barnett@gmail.com>
2019-11-22 23:26:03 -0600

    change port

--- src/router.jsx
+++ src/router.jsx
@@ -14,7 +14,7 @@ import pleasePrint from './pages/pleasePrint';
 import Member from './components/Member';
 
 render ((
-    <HashRouter basename="/">
+    <HashRouter basename="/:8080">
         <Switch>
             <Route exact path="/" component={home} />
             <Route path="/about" component={about} />
