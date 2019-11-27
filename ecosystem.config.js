618259479fd3339f1cc95e5f324d867cdf921e02
RashadB88 <Rashad.barnett@gmail.com>
2019-11-27 12:21:58 -0600

    Test Cron

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,13 +10,13 @@ module.exports = {
     env_production: { NODE_ENV: 'production' }
   },
   {
-    name: 'test',
+    name: 'CryptoplexCron',
     script: '/home/rashadbahamas/www/cryptoplexCronJob/ms.js',
     instances: 1,
     autorestart: true,
     watch: true,
     max_memory_restart: '1G',
-    cron_restart : '* * * * *',
+    cron_restart : '0 0 * * *',
     env: { NODE_ENV: 'development' },
     env_production: { NODE_ENV: 'production' }
   }]
