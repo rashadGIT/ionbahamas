84fa74f813a318c7b1007a70ae1e1cfa2318c9f0
RashadB88 <Rashad.barnett@gmail.com>
2019-11-30 20:40:49 -0600

    ENV

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
     autorestart: true,
     watch: true,
     max_memory_restart: '1G',
-    cron_restart : '0 0 * * *',
+    //cron_restart : '0 0 * * *',
     env: { NODE_ENV: 'development' },
     env_production: { NODE_ENV: 'production' }
   }]
