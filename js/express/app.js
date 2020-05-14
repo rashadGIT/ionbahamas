73258263ea6f9b3caf383dd301137fa48c374506
rashadGIT <rashad.barnett@gmail.com>
2020-05-14 00:16:31 -0600

    http express working

--- js/express/app.js
+++ js/express/app.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const app = express()
-const https = require('https');
+var express = require('express')
+var fs = require('fs')
+var http = require('http');
+var https = require('https')
+var app = express()
 const port = 3500
 var myCar = {
     make: 'Ford',
@@ -8,10 +10,19 @@ var myCar = {
     year: 1969
 };
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
 
 app.get('/fire', (req, res) => res.send(myCar))
 
-//app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
-
-https.createServer(app).listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+var httpServer = http.createServer(app)
+.listen(port);
+;
+https.createServer({
+  key: fs.readFileSync('/home/ionbahamasnet/ssl/keys/cd563_82615_023b7e6433049e6d1e3fae2f3c33b9fe.key'),
+  cert: fs.readFileSync('/home/ionbahamasnet/ssl/certs/ionbahamas_net_cd563_82615_1620061939_a7c8589ce2a7e5b79d0a624ee5c86861.crt')
+}, app)
+.listen(3000, function () {
+  console.log('Example app listening on port 3000! Go to https://localhost:3000/')
+})
