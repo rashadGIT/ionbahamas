3eda45e20713bb508bbaae74d213d11e992553a6
rashadGIT <Rashad.Barnett@gmail.com>
2020-05-15 22:36:11 -0500

    express test

--- js/express/server.js
+++ js/express/server.js
@@ -1,28 +1,22 @@
 var express = require('express')
+var bodyParser=require("body-parser");
 var fs = require('fs')
 var http = require('http');
 var https = require('https')
+var util = require('./util.js')
 var app = express()
-const port = 3500
-var myCar = {
-    make: 'Ford',
-    model: 'Mustang',
-    year: 1969
-};
+const httpPort = 3000
+const httpsPort = 3500
 
-app.get('/', function (req, res) {
-  res.send('hello world')
-})
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/util', util);
 
-app.get('/fire', (req, res) => res.send(myCar))
-
-var httpServer = http.createServer(app)
-.listen(port);
-;
+http.createServer(app).listen(httpPort);
 https.createServer({
   key: fs.readFileSync('/home/ionbahamasnet/ssl/keys/cd563_82615_023b7e6433049e6d1e3fae2f3c33b9fe.key'),
   cert: fs.readFileSync('/home/ionbahamasnet/ssl/certs/ionbahamas_net_cd563_82615_1620061939_a7c8589ce2a7e5b79d0a624ee5c86861.crt')
 }, app)
-.listen(3000, function () {
-  console.log('Example app listening on port 3000! Go to https://localhost:3000/')
+.listen(httpsPort, () => {
+  console.log(`Example app listening on port ${httpsPort}! Go to https://localhost:${httpsPort}/'`)
 })
