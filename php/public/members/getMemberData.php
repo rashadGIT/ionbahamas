b6d4524fc982f1865888a5134eb006cd1000a411
RashadB88 <Rashad.barnett@gmail.com>
2020-08-18 22:05:14 -0500

    Clean up unused files

--- php/public/members/getMemberData.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require '../util/header.php';
-
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        $cmd = sprintf("node ../../../js/node/members/getMembershipData.js");
-        echo shell_exec("$cmd 2>&1");
-    }
-?>
--- php/public/util/header.php
+++
@@ -1,6 +0,0 @@
-<?php
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-    header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization');
-    header('Content-Type:application/json');
-?>
